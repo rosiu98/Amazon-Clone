65c77eee12c89e3eb768a77e3317e55ad4d74f54
Paweł <rosiu978@gmail.com>
2021-06-11 20:13:34 +0200

    adding

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -3,6 +3,20 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 export default async (req, res) => {
     const {items,email} = req.body;
 
-    console.log(items)
-    console.log(email)
+    const transformedItems = items.map(item => ({
+        description: item.description,
+        quantity: 1,
+        price_data: {
+            currency: 'pln',
+            unit_amount: item.price * 100,
+            product_data: {
+                name: item.title,
+                images: [item.image]
+            },
+        }
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+        
+    })
 }   
