b4493d7d8f18522ef6389bbb3d1e83692dadc766
Paweł <rosiu978@gmail.com>
2021-06-11 14:20:39 +0200

    adding basket functionality with redux and auth with firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAovY9wZgXhv4XQtTk1ejY1rirH_2O1lXw",
+  authDomain: "clone-e2546.firebaseapp.com",
+  projectId: "clone-e2546",
+  storageBucket: "clone-e2546.appspot.com",
+  messagingSenderId: "693007271400",
+  appId: "1:693007271400:web:3dd5f86d6db2d61d53905d"
+};
